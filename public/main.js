dd0910a612a6894de76817762f934eb01d722fe4
Angie Gonzalez <angiem.go@gmail.com>
2022-11-27 22:10:33 -0600

    made button work

--- public/main.js
+++ public/main.js
@@ -1,20 +1,28 @@
 import 'bootstrap';
 import '../styles/main.scss';
-import getjoke from '../api/jokepromise';
+import jokePromise from '../api/jokepromise';
 
 const init = () => {
   document.querySelector('#app').innerHTML = `
-    <h1>HELLO! Angie's Yoke Sh!t</h1>
-    <button class="btn btn-danger" id="click-me">Click ME!</button>
+  <h1> Let's Yoke Around </h1>
+  <div class = "joke" id = "the-setup" ></div>
+  <button class="btn btn-danger" id="click-me">Get a Punny Joke!</button>
+  <div id = "the-punch-line"></div>
   `;
 };
 init();
 
-const jokefunc = () => {
-  getjoke().then((mytaco) => {
+const theButton = document.querySelector('#click-me');
+const theSetup = document.querySelector('#the-setup'); // jokeText
+const thePunchLine = document.querySelector('#the-punch-line');
+
+const getJoke = () => {
+  jokePromise().then((mytaco) => {
     console.warn(mytaco.setup);
     console.warn(mytaco.delivery);
+    theSetup.innerHTML = mytaco.setup;
+    thePunchLine.innerHTML = mytaco.delivery;
   });
 };
 
-document.querySelector('#click-me').addEventListener('click', jokefunc);
+theButton.addEventListener('click', getJoke);
